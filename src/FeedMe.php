f01ab93b6e6c44892f91a76e9b10e6af62c71dcd
albar <mubarrid.s@students.amikom.ac.id>
2016-09-27 11:55:01 +0700

    add articles method

--- src/FeedMe.php
+++ src/FeedMe.php
@@ -5,25 +5,26 @@ use Symfony\Component\DomCrawler\Crawler;
 
 class FeedMe
 {
-    /**
-     * @var FeedDriver
-     */
     protected $driver;
 
-    public function __construct($driver)
+    public function __construct(FeedDriver $driver)
     {
         $this->driver = $driver;
     }
 
-    /**
-     * @return FeedDriver
-     */
     public function load()
     {
         $response = file_get_contents($this->driver->url());
 
         $this->driver->setData(new Crawler($response));
 
-        return $this->driver->generate();
+        $this->driver->generate();
+
+        return $this;
+    }
+
+    public function articles()
+    {
+        return $this->driver->articles();
     }
 }
